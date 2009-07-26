0ac2cb93d0e190fb8545eedd0e3e1f40b5677462
motemen <motemen@gmail.com>
2009-07-26 23:46:59 +0900

    initial commit

---
+++ lib/Web/Scrape/Configure.pm
@@ -0,0 +1,33 @@
+package Web::Scrape::Configure;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Web::Scrape::Configure -
+
+=head1 SYNOPSIS
+
+  use Web::Scrape::Configure;
+
+=head1 DESCRIPTION
+
+Web::Scrape::Configure is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
