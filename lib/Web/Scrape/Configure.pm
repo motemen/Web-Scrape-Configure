abfa39510cb04a8129beca99e410cdd681b0a2f3
motemen <motemen@gmail.com>
2009-07-27 00:07:16 +0900

    config

--- lib/Web/Scrape/Configure.pm
+++ lib/Web/Scrape/Configure.pm
@@ -1,10 +1,88 @@
 package Web::Scrape::Configure;
-
 use strict;
 use warnings;
+use base 'WWW::Mechanize';
+
 our $VERSION = '0.01';
 
+sub configure {
+    my $self = shift;
+    $self->{config} = { @_ };
+}
+
+sub process {
+    my ($self, $uri) = @_;
+    $uri = URI->new($uri) unless ref $uri;
+
+    my $config = $self->_site_config($uri) or return;
+    my $control = $config->{control};
+
+    $self->get($uri);
+    
+    my $result;
+    foreach my $key (keys %$config) {
+        next if $key eq 'control';
+        my ($method, $arg) = %{ $config->{$key} };
+        my @values = map $_->string_value, $self->$method($arg);
+        my $value = ($key =~ s/\[\]$// ? \@values : $values[0]);
+        $result->{$key} = $value;
+    }
+    $result;
+}
+
+sub _host_config {
+    my ($self, $uri) = @_;
+    $uri = URI->new($uri) unless ref $uri;
+    my $host = $uri->host;
+    my @parts = split /\./, $host;
+    while (@parts) {
+        my $config = $self->{config}->{ join '.', @parts };
+        return $config if $config;
+        shift @parts;
+    }
+}
+
+sub _site_config {
+    my ($self, $uri) = @_;
+    $uri = URI->new($uri) unless ref $uri;
+    my $config = $self->_host_config($uri) or return;
+    foreach (keys %$config) {
+        return $config->{$_} if $uri->path =~ qr/$_/;
+    }
+}
+
+use Encode;
+use HTML::Selector::XPath;
+use HTML::TreeBuilder::XPath;
+
+sub WWW::Mechanize::selector {
+    my ($mech, $selector) = @_;
+    $mech->xpath(HTML::Selector::XPath->new($selector)->to_xpath);
+}
+sub WWW::Mechanize::xpath {
+    my ($mech, $xpath) = @_;
+ 
+    my @ct = $mech->response->header('Content-Type');
+ 
+    my $content = $mech->content;
+    unless (Encode::is_utf8($content)) {
+        if ($ct[0] && $ct[0] =~ /charset=([\w\-]+)/) {
+            $content = decode($1, $mech->content);
+        } else {
+            $content = decode_utf8($mech->content);
+        }
+    }
+ 
+    my $tree = HTML::TreeBuilder::XPath->new;
+    $tree->parse($content);
+    $tree->eof;
+ 
+    my @nodes = $tree->findnodes($xpath);
+    return wantarray ? @nodes : $nodes[0];
+}
+
 1;
+
 __END__
 
 =head1 NAME
---
+++ t/01_config.t
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use Test::More tests => 4;
+use Web::Scrape::Configure;
+
+my $scraper = Web::Scrape::Configure->new;
+$scraper->configure(
+    '2chan.net' => {
+        qr</b/res/\d+\.htm$> => {
+            image => { xpath => '//form/a/img/../@href' },
+        },
+    },
+
+    'pixiv.net' => {
+        qr</member_illust\.php\?mode=medium&illust_id=\d+> => {
+            control => {
+                follow => { xpath => 'id("content2")//a/img/../@href' },
+            },
+            'tags[]' => { xpath => 'id("tag_area_")//a/text()' },
+        },
+        qr</member_illust\.php\?mode=big&illust_id=\d+> => {
+            image => { xpath => '//img/@src' },
+        },
+    },
+);
+
+ok my $config = $scraper->_site_config('http://img.2chan.net/b/res/65778369.htm'), 'existent site';
+ok $config->{image};
+ok $config->{image}->{xpath};
+
+ok !$scraper->_site_config('http://nonexistentsite/'), 'nonexistent site';
+
